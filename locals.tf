3ef226c73445614d88ca04b2662e4861ab3aacc6
Tarunteja <taruntejamahanthi@gmail.com>
2026-03-02 00:42:03 +0530

    vpc-module-2

--- locals.tf
+++ locals.tf
@@ -4,5 +4,10 @@ locals{
         evn = var.environment
         terraform = "true"
     }
-    vpc_final_tags =  merge(local.common_tags,${var.project}-${var.environment},var.vpc_tags)
+    vpc_final_tags =  merge(
+                            local.common_tags,
+                            {
+                                Name = "${var.project}-${var.environment}"
+                            },
+                            var.vpc_tags)
 }
