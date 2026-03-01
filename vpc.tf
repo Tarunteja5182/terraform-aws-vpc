b6644b9ddf4e0ff272a78e8bf7beb195d1fc90a6
Tarunteja <taruntejamahanthi@gmail.com>
2026-03-02 01:01:02 +0530

    vpc-module-3

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "mod_vpc"{
-    cidr_block= var.cidr_blocks
+    cidr_block= var.vpc_cidr
     instance_tenancy = "default"
     enable_dns_hostnames= "true"
     tags = local.vpc_final_tags
