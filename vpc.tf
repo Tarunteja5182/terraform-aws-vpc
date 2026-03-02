c741a1dbfbd31684ba9f6af1bc72612275a45570
Tarunteja <taruntejamahanthi@gmail.com>
2026-03-03 00:38:29 +0530

    route table and associations 1

--- vpc.tf
+++ vpc.tf
@@ -45,3 +45,62 @@ resource "aws_subnet" "database" {
     Name = "roboshop-dev"
   }
 }
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.mod_vpc.id
+
+  tags = {
+    Name = "public-route"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.example.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
+
+resource "aws_eip" "eip" {
+  domain                    = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "Roboshop-gw-NAT"
+  }
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.example.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_internet_gateway.ngw.id
+}
+
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.example.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.ngw.id
+}
+
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidr)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidr)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.example.id
+}
