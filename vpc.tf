ef07c2733f4d9cc9f241b0a0416e654a2c62b6eb
Tarunteja <taruntejamahanthi@gmail.com>
2026-03-03 01:08:03 +0530

    routetable 5

--- vpc.tf
+++ vpc.tf
@@ -71,7 +71,7 @@ resource "aws_route_table" "database" {
 }
 
 resource "aws_route" "public" {
-  route_table_id            = aws_route_table.example.id
+  route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.gw.id
 }
@@ -91,13 +91,13 @@ resource "aws_nat_gateway" "ngw" {
 }
 
 resource "aws_route" "private" {
-  route_table_id            = aws_route_table.example.id
+  route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.ngw.id
 }
 
 resource "aws_route" "database" {
-  route_table_id            = aws_route_table.example.id
+  route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_nat_gateway.ngw.id
 }
